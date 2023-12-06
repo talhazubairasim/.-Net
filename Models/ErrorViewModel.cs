3c36f6edd21393094a6551ff9ee264edabe9cd3f
talha.zubair <talha.zubair@ISB-DT-TALHAZ>
2023-12-06 14:41:18 +0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prep.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
